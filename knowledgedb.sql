aa4d8be1546ad6dfdb60af80a2c1f2806a85034e
M Andri Eka F <13511088@std.stei.itb.ac.id>
2014-07-07 14:47:00 +0700

    new sql dump

--- knowledgedb.sql
+++ knowledgedb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 01, 2014 at 04:08 AM
+-- Generation Time: Jul 07, 2014 at 09:45 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_data` (
-  `no` int(255) NOT NULL AUTO_INCREMENT,
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `no_br` varchar(50) NOT NULL,
   `cr_number` varchar(50) NOT NULL,
   `status` int(100) NOT NULL,
@@ -38,21 +38,26 @@ CREATE TABLE IF NOT EXISTS `tbl_data` (
   `IT_testing_PIC` int(100) NOT NULL,
   `request_date` date NOT NULL,
   `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
+  `end_date` date DEFAULT NULL,
   `key_achievement` int(11) NOT NULL,
-  `month_of_register` date NOT NULL,
-  PRIMARY KEY (`no`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `month_of_register` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `tbl_data`
 --
 
-INSERT INTO `tbl_data` (`no`, `no_br`, `cr_number`, `status`, `reflex`, `application_name`, `user`, `departement_PIC`, `IT_testing_PIC`, `request_date`, `start_date`, `end_date`, `key_achievement`, `month_of_register`) VALUES
-(2, 'eaddad', 'yadgayd', 1, 'test', 'adaeda', 1, 'adedead', 1, '2014-06-08', '2014-06-02', '2014-06-03', 1, '2014-06-16'),
-(3, '223', 'sdadw', 2, 'test', 'apa', 1, 'dwdw', 2, '0000-00-00', '0000-00-00', '0000-00-00', 1, '0000-00-00'),
-(4, 'de', 'ded', 3, 'eded', 'eded', 50, 'wsw', 2, '0000-00-00', '0000-00-00', '0000-00-00', 1, '0000-00-00'),
-(5, '12', '232', 1, 'wwdwd', 'dwdwdw', 56, 'sdsds', 2, '0000-00-00', '0000-00-00', '0000-00-00', 1, '0000-00-00');
+INSERT INTO `tbl_data` (`id`, `no_br`, `cr_number`, `status`, `reflex`, `application_name`, `user`, `departement_PIC`, `IT_testing_PIC`, `request_date`, `start_date`, `end_date`, `key_achievement`, `month_of_register`) VALUES
+(2, 'eaddad', 'yadgayd', 1, 'test', 'adaeda', 1, 'adedead', 1, '2014-06-08', '2014-06-02', '2014-06-03', 1, '07.2014'),
+(3, '223', 'sdadw', 2, 'test', 'apa', 1, 'dwdw', 2, '0000-00-00', '0000-00-00', '0000-00-00', 1, '10.2014'),
+(4, 'de', 'ded', 3, 'eded', 'eded', 1, 'wsw', 2, '0000-00-00', '0000-00-00', '0000-00-00', 1, '09.2014'),
+(5, '12', '232', 1, 'wwdwd', 'dwdwdw', 1, 'sdsds', 2, '0000-00-00', '0000-00-00', '0000-00-00', 1, '06.2015'),
+(6, 'wda', 'wdw', 1, 'dwad', 'wdwd', 1, 'sdsdadas', 1, '2014-07-14', '2014-07-17', '2014-07-04', 1, '07.2014'),
+(7, '3434', 'rfrf', 1, 'frfrfrf', 'test', 2, 'rfrfr', 1, '2014-07-04', '2014-07-04', '2014-07-01', 1, '10.2014'),
+(8, '213', 'wwe2', 1, 'test', 'swsw', 3, 'wswwd', 1, '2014-07-16', '2014-07-17', '2014-07-18', 1, '12.2014'),
+(9, 'brq', 'r555', 1, 'test', 'tesst2', 1, 'tst', 1, '2014-07-04', '2014-07-01', '0000-00-00', 0, '07.2014');
 
 -- --------------------------------------------------------
 
@@ -68,14 +73,26 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`username`),
   UNIQUE KEY `nama` (`nama`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `tbl_user`
 --
 
 INSERT INTO `tbl_user` (`id`, `username`, `password`, `nama`) VALUES
-(1, 'emon', 'ganteng', 'andri');
+(1, 'emon', 'ganteng', 'andri'),
+(2, 'bobo', 'bobo', 'bobo'),
+(3, 'test1', 'test1', 'test1');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_data`
+--
+ALTER TABLE `tbl_data`
+  ADD CONSTRAINT `user.id` FOREIGN KEY (`user`) REFERENCES `tbl_user` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
