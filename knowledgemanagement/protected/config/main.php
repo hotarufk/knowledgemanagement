7556253f89e89a56c79fed6ad463e2f8caabe69f
Fazkool <fajarghosty@gmail.com>
2014-07-18 14:12:34 +0700

    Index temporary

--- knowledgemanagement/protected/config/main.php
+++ knowledgemanagement/protected/config/main.php
@@ -12,7 +12,7 @@ return array(
     'theme'=>'bootstrap', // requires you to copy the theme under your themes directory
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Web Application',
-	'defaultController' => 'data/index', 
+	'defaultController' => 'site/index', 
 	// preloading 'log' component
 	'preload'=>array('log'),
 
