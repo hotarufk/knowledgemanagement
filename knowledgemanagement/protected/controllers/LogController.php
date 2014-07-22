7f5cc15df405438695a6ef21d49bb5da6b9d2c28
M Andri Eka F <13511088@std.stei.itb.ac.id>
2014-07-22 14:42:35 +0700

    additional rule on all controller rule only authorized people who can access website

--- knowledgemanagement/protected/controllers/LogController.php
+++ knowledgemanagement/protected/controllers/LogController.php
@@ -28,16 +28,9 @@ class LogController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'actions'=>array('index','view','update','admin'),
+				//'users'=>array('*'),
+				'roles'=>array('admin'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
--- knowledgemanagement/protected/controllers/UserController.php
+++ knowledgemanagement/protected/controllers/UserController.php
@@ -27,19 +27,20 @@ class UserController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
+			// array('allow',  // allow all users to perform 'index' and 'view' actions
+				// 'actions'=>array('index','view'),
+				// 'users'=>array('*'),
+			// ),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
+				'actions'=>array('create','update','index','view','admin','delete'),
 				//'users'=>array('@'),
 				'roles'=>array('admin'),
 			),
+			// array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				// 'actions'=>array('admin','delete'),
+				// //'users'=>array('@'),
+				// 'roles'=>array('admin'),
+			// ),
 			array('deny',  // deny all users
 				'users'=>array('*'),
 			),
