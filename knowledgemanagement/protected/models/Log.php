0bf454017fb0c64be19ae994c7faf99e8863ab20
M Andri Eka F <13511088@std.stei.itb.ac.id>
2014-08-22 10:25:47 +0700

    pindahin yii nya ke folder knowledge management

--- knowledgemanagement/protected/models/Log.php
+++ knowledgemanagement/protected/models/Log.php
@@ -47,7 +47,7 @@ class Log extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-			'user0' => array(self::BELONGS_TO, 'TblUser', 'user'),
+			//'user0' => array(self::BELONGS_TO, 'TblUser', 'user'),
 		);
 	}
 
